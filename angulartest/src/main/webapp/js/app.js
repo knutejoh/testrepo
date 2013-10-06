621714f54828ee4b0684fffeb359ebd5eda75024
knut-erik.johnsen <knut-erik.johnsen@192.168.56.1>
2013-10-06 23:58:30 +0200

    Lagt til litt bootstrap elementer. Begynt litt med egen CSS til lotto-tallene. Ting er knotete, men går fremover.

--- angulartest/src/main/webapp/js/app.js
+++ angulartest/src/main/webapp/js/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-angular.module('helloworld', ['restTestServices', 'restTestControllers']);
+angular.module('helloworld', ['restTestServices', 'restTestControllers', 'ui.bootstrap'])
+
+
+    .config(function($routeProvider) {
+         $routeProvider.
+            when('/', {controller:'RestController', templateUrl:'templates/sjefen.html'}).
+            when('/test/', {controller:'PhoneListCtrl', templateUrl:'templates/phones.html'}).
+            otherwise({redirectTo:'/'});
+
+    })
+
+
+;
