0249ab8c735a44fcd21f6d3cefe3ed72c05e554d
knut-erik.johnsen <knut-erik.johnsen@192.168.56.1>
2014-01-07 23:51:52 +0100

    Endret til PhantomJS

--- tooltest/Gruntfile.js
+++ tooltest/Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function (grunt) {
       options: {
         port: 8080,
         // Change this to '0.0.0.0' to access the server from outside. localhost
-        hostname: '192.168.0.4',
+        hostname: '127.0.0.1',
         livereload: 35729
       },
       livereload: {
